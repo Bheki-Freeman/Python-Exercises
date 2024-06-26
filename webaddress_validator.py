4df7742caa3155401ab409d32a4552bcb4c997f0
Bheki-Freeman <bhekisisadlamini05@gmail.com>
2024-06-26 12:26:33 +0200

    create web_address validator module

---
+++ webaddress_validator.py
@@ -0,0 +1,25 @@
+# I am creating a simple web validator program
+import re
+
+def web_address_validator(web_address:str) -> bool:
+    pattern = re.compile("^[w]{3}.[^#@.*%&!]+.[a-zA-Z]{3}$")
+    if pattern.search(web_address) != None:
+        return True
+    else:
+        return False
+
+def main() -> None:
+    address = 'www.google.com'
+    while address != '.q':
+        address = input("Enter address to check, .q to QUIT: ")
+        if address == '.q':
+            break
+        if web_address_validator(address):
+            print(f'ACCESS GRANTED!')
+        else:
+           print(f'ACCESS DENIED!')
+
+# ---------------------------
+if __name__=='__main__':
+    main()
+
